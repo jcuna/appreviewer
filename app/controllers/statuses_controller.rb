432e74ece7c52b35eac5d79546c8f0a0d9dfef6d
Jon Garcia <garciajon@me.com>
2013-10-28 17:06:10 -0400

    changed notice to say review instaead of status

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -44,7 +44,7 @@ class StatusesController < ApplicationController
 
     respond_to do |format|
       if @status.save
-        format.html { redirect_to @status, notice: 'Status was successfully created.' }
+        format.html { redirect_to @status, notice: 'Your review has been submitted.' }
         format.json { render json: @status, status: :created, location: @status }
       else
         format.html { render action: "new" }
