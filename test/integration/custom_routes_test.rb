345a5a2bc0c8286b5b4f66d991ef6719be51eb02
Jon Garcia <garciajon@me.com>
2013-10-30 22:23:06 -0400

    Created routes test

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+ test "that the /login route opens the login page" do
+ 	get '/login'
+ 	assert_response :success
+end
+
+
+ test "that the /register route opens the register page" do
+ 	get '/register'
+ 	assert_response :success
+end
+
+ test "that the /reviews route opens the reviews page" do
+ 	get '/reviews'
+ 	assert_response :success
+end
+
+ test "that the /newreview route opens the new review page" do
+ 	get '/newreview'
+ 	assert_response :success
+end
+
+end
